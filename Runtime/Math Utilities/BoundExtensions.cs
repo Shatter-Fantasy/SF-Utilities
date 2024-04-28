0335bfdbcb25663aca09d903ff2cd17dfb1c3343
crowhound <jonathan.westfall1@gmail.com>
2024-04-28 17:33:59 -0500

    Added the BoundsInt set of corner utility functions.

--- Runtime/Math Utilities/BoundExtensions.cs
+++ Runtime/Math Utilities/BoundExtensions.cs
@@ -16,6 +16,18 @@ namespace SF.Utilities
         public static Vector2 MiddleRight(this Bounds bounds) => new(bounds.max.x, bounds.center.y);
         public static Vector2 MiddleCenter(this Bounds bounds) => new(bounds.center.x, bounds.center.y);
         public static Vector2 MiddleLeft(this Bounds bounds) => new(bounds.min.x, bounds.center.y);
+
+        public static Vector2 TopRight(this BoundsInt bounds) => new(bounds.max.x, bounds.max.y);
+        public static Vector2 TopCenter(this BoundsInt bounds) => new(bounds.center.x, bounds.max.y);
+        public static Vector2 TopLeft(this BoundsInt bounds) => new(bounds.min.x, bounds.max.y);
+
+        public static Vector2 BottomRight(this BoundsInt bounds) => new(bounds.max.x, bounds.min.y);
+        public static Vector2 BottomCenter(this BoundsInt bounds) => new(bounds.center.x, bounds.min.y);
+        public static Vector2 BottomLeft(this BoundsInt bounds) => new(bounds.min.x, bounds.min.y);
+
+        public static Vector2 MiddleRight(this BoundsInt bounds) => new(bounds.max.x, bounds.center.y);
+        public static Vector2 MiddleCenter(this BoundsInt bounds) => new(bounds.center.x, bounds.center.y);
+        public static Vector2 MiddleLeft(this BoundsInt bounds) => new(bounds.min.x, bounds.center.y);
         #endregion
     }
 }
