c589bd546fe93e77c83f8b4a764abcdf33e0ca2a
crowhound <jonathan.westfall1@gmail.com>
2024-04-28 19:43:56 -0500

    Started adding some of the editor only methods for Grid Meshes.

--- Runtime/Common/Graphics Library/GlUtilities.cs
+++ Runtime/Common/Graphics Library/GlUtilities.cs
@@ -1,3 +1,7 @@
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -7,6 +11,9 @@ namespace SF.Utilities
     {
         public static Color GLColor;
         public static Material DrawHandleMaterial;
+
+        private const int k_GridGizmoVertexCount = 32000;
+        private const float k_GridGizmoDistanceFalloff = 50f;
 
         public static void InitHandleMaterial(CompareFunction zTest = CompareFunction.Always)
         {
@@ -58,6 +65,21 @@ namespace SF.Utilities
             GL.Vertex3(p1.x, p1.y, p1.z);
             GL.Vertex3(p2.x, p2.y, p2.z);
         }
+        public static void DrawBatchedHorizontalLine(float x1, float x2, float y)
+        {
+            GL.Vertex3(x1, y, 0f);
+            GL.Vertex3(x2, y, 0f);
+            GL.Vertex3(x2, y + 1, 0f);
+            GL.Vertex3(x1, y + 1, 0f);
+        }
+        public static void DrawBatchedVerticalLine(float y1, float y2, float x)
+        {
+            GL.Vertex3(x, y1, 0f);
+            GL.Vertex3(x, y2, 0f);
+            GL.Vertex3(x + 1, y2, 0f);
+            GL.Vertex3(x + 1, y1, 0f);
+        }
+
         public static void DrawBox(Rect position, int width = 1)
         {
             var points0 = new Vector3(position.xMin, position.yMin, 0f);
@@ -117,7 +139,6 @@ namespace SF.Utilities
                     break;
             }
         }
-
         public static void DrawSelectedHexGridArea(GridLayout gridLayout, BoundsInt area, Color color)
         {
             int requiredVertices = 4 * (area.size.x + area.size.y) - 2;
@@ -181,7 +202,278 @@ namespace SF.Utilities
             GL.PopMatrix();
         }
 
+#if UNITY_EDITOR
 
+        public static void DrawGridGizmo(GridLayout gridLayout, Transform transform, Color color, ref Mesh gridMesh, ref Material gridMaterial)
+        {
+            // TODO: Hook this up with DrawGrid
+            if(Event.current.type != EventType.Repaint)
+                return;
+
+            if(gridMesh == null)
+                gridMesh = GenerateCachedGridMesh(gridLayout, color);
+
+            if(gridMaterial == null)
+            {
+                gridMaterial = (Material)EditorGUIUtility.LoadRequired("SceneView/GridGap.mat");
+            }
+
+            if(gridLayout.cellLayout == GridLayout.CellLayout.Hexagon)
+            {
+                gridMaterial.SetVector("_Gap", new Vector4(1f, 1f / 3f, 1f, 1f));
+                gridMaterial.SetVector("_Stride", new Vector4(1f, 1f, 1f, 1f));
+            }
+            else
+            {
+                gridMaterial.SetVector("_Gap", gridLayout.cellSize);
+                gridMaterial.SetVector("_Stride", gridLayout.cellGap + gridLayout.cellSize);
+            }
+
+            gridMaterial.SetPass(0);
+            ApplyHandleMaterial(CompareFunction.Always);
+            GL.PushMatrix();
+            if(gridMesh.GetTopology(0) == MeshTopology.Lines)
+                GL.Begin(GL.LINES);
+            else
+                GL.Begin(GL.QUADS);
+
+            Graphics.DrawMeshNow(gridMesh, transform.localToWorldMatrix);
+            GL.End();
+            GL.PopMatrix();
+        }
+
+        private static Mesh GenerateCachedGridMesh(GridLayout gridLayout, Color color)
+        {
+            switch(gridLayout.cellLayout)
+            {
+                case GridLayout.CellLayout.Hexagon:
+                    return GenerateCachedHexagonalGridMesh(gridLayout, color);
+                case GridLayout.CellLayout.Isometric:
+                case GridLayout.CellLayout.IsometricZAsY:
+                case GridLayout.CellLayout.Rectangle:
+                    int min = k_GridGizmoVertexCount / -32;
+                    int max = min * -1;
+                    int numCells = max - min;
+                    RectInt bounds = new RectInt(min, min, numCells, numCells);
+
+                    return GenerateCachedGridMesh(gridLayout, color, 0f, bounds, MeshTopology.Lines);
+            }
+            return null;
+        }
+        public static Mesh GenerateCachedGridMesh(GridLayout gridLayout, Color color, float screenPixelSize, RectInt bounds, MeshTopology topology)
+        {
+            Mesh mesh = new Mesh();
+            mesh.hideFlags = HideFlags.HideAndDontSave;
+
+            int vertex = 0;
+
+            int totalVertices = topology == MeshTopology.Quads ?
+                8 * (bounds.size.x + bounds.size.y) :
+                4 * (bounds.size.x + bounds.size.y);
+
+            Vector3 horizontalPixelOffset = new Vector3(screenPixelSize, 0f, 0f);
+            Vector3 verticalPixelOffset = new Vector3(0f, screenPixelSize, 0f);
+
+            Vector3[] vertices = new Vector3[totalVertices];
+            Vector2[] uvs2 = new Vector2[totalVertices];
+
+            Vector3 cellStride = gridLayout.cellSize + gridLayout.cellGap;
+            Vector3Int minPosition = new Vector3Int(0, bounds.min.y, 0);
+            Vector3Int maxPosition = new Vector3Int(0, bounds.max.y, 0);
+
+            Vector3 cellGap = Vector3.zero;
+            if(!Mathf.Approximately(cellStride.x, 0f))
+            {
+                cellGap.x = gridLayout.cellSize.x / cellStride.x;
+            }
+
+            for(int x = bounds.min.x; x < bounds.max.x; x++)
+            {
+                minPosition.x = x;
+                maxPosition.x = x;
+
+                vertices[vertex + 0] = gridLayout.CellToLocal(minPosition);
+                vertices[vertex + 1] = gridLayout.CellToLocal(maxPosition);
+                uvs2[vertex + 0] = Vector2.zero;
+                uvs2[vertex + 1] = new Vector2(0f, cellStride.y * bounds.size.y);
+                if(topology == MeshTopology.Quads)
+                {
+                    vertices[vertex + 2] = gridLayout.CellToLocal(maxPosition) + horizontalPixelOffset;
+                    vertices[vertex + 3] = gridLayout.CellToLocal(minPosition) + horizontalPixelOffset;
+                    uvs2[vertex + 2] = new Vector2(0f, cellStride.y * bounds.size.y);
+                    uvs2[vertex + 3] = Vector2.zero;
+                }
+                vertex += topology == MeshTopology.Quads ? 4 : 2;
+
+                vertices[vertex + 0] = gridLayout.CellToLocalInterpolated(minPosition + cellGap);
+                vertices[vertex + 1] = gridLayout.CellToLocalInterpolated(maxPosition + cellGap);
+                uvs2[vertex + 0] = Vector2.zero;
+                uvs2[vertex + 1] = new Vector2(0f, cellStride.y * bounds.size.y);
+                if(topology == MeshTopology.Quads)
+                {
+                    vertices[vertex + 2] = gridLayout.CellToLocalInterpolated(maxPosition + cellGap) + horizontalPixelOffset;
+                    vertices[vertex + 3] = gridLayout.CellToLocalInterpolated(minPosition + cellGap) + horizontalPixelOffset;
+                    uvs2[vertex + 2] = new Vector2(0f, cellStride.y * bounds.size.y);
+                    uvs2[vertex + 3] = Vector2.zero;
+                }
+                vertex += topology == MeshTopology.Quads ? 4 : 2;
+            }
+
+            minPosition = new Vector3Int(bounds.min.x, 0, 0);
+            maxPosition = new Vector3Int(bounds.max.x, 0, 0);
+            cellGap = Vector3.zero;
+            if(!Mathf.Approximately(cellStride.y, 0f))
+            {
+                cellGap.y = gridLayout.cellSize.y / cellStride.y;
+            }
+
+            for(int y = bounds.min.y; y < bounds.max.y; y++)
+            {
+                minPosition.y = y;
+                maxPosition.y = y;
+
+                vertices[vertex + 0] = gridLayout.CellToLocal(minPosition);
+                vertices[vertex + 1] = gridLayout.CellToLocal(maxPosition);
+                uvs2[vertex + 0] = Vector2.zero;
+                uvs2[vertex + 1] = new Vector2(cellStride.x * bounds.size.x, 0f);
+                if(topology == MeshTopology.Quads)
+                {
+                    vertices[vertex + 2] = gridLayout.CellToLocal(maxPosition) + verticalPixelOffset;
+                    vertices[vertex + 3] = gridLayout.CellToLocal(minPosition) + verticalPixelOffset;
+                    uvs2[vertex + 2] = new Vector2(cellStride.x * bounds.size.x, 0f);
+                    uvs2[vertex + 3] = Vector2.zero;
+                }
+                vertex += topology == MeshTopology.Quads ? 4 : 2;
+
+                vertices[vertex + 0] = gridLayout.CellToLocalInterpolated(minPosition + cellGap);
+                vertices[vertex + 1] = gridLayout.CellToLocalInterpolated(maxPosition + cellGap);
+                uvs2[vertex + 0] = Vector2.zero;
+                uvs2[vertex + 1] = new Vector2(cellStride.x * bounds.size.x, 0f);
+                if(topology == MeshTopology.Quads)
+                {
+                    vertices[vertex + 2] = gridLayout.CellToLocalInterpolated(maxPosition + cellGap) + verticalPixelOffset;
+                    vertices[vertex + 3] = gridLayout.CellToLocalInterpolated(minPosition + cellGap) + verticalPixelOffset;
+                    uvs2[vertex + 2] = new Vector2(cellStride.x * bounds.size.x, 0f);
+                    uvs2[vertex + 3] = Vector2.zero;
+                }
+                vertex += topology == MeshTopology.Quads ? 4 : 2;
+            }
+
+            var uv0 = new Vector2(k_GridGizmoDistanceFalloff, 0f);
+            var uvs = new Vector2[vertex];
+            var indices = new int[vertex];
+            var colors = new Color[vertex];
+            var normals = new Vector3[totalVertices];     // Normal channel stores the position of the other end point of the line.
+            var uvs3 = new Vector2[totalVertices];        // UV3 channel stores the UV2 value of the other end point of the line.
+
+            for(int i = 0; i < vertex; i++)
+            {
+                uvs[i] = uv0;
+                indices[i] = i;
+                colors[i] = color;
+                var alternate = i + ((i % 2) == 0 ? 1 : -1);
+                normals[i] = vertices[alternate];
+                uvs3[i] = uvs2[alternate];
+            }
+
+            mesh.vertices = vertices;
+            mesh.uv = uvs;
+            mesh.uv2 = uvs2;
+            mesh.uv3 = uvs3;
+            mesh.colors = colors;
+            mesh.normals = normals;
+            mesh.SetIndices(indices, topology, 0);
+
+            return mesh;
+        }
+        private static Mesh GenerateCachedHexagonalGridMesh(GridLayout gridLayout, Color color)
+        {
+            Mesh mesh = new Mesh();
+            mesh.hideFlags = HideFlags.HideAndDontSave;
+            int vertex = 0;
+            int max = k_GridGizmoVertexCount / (2 * (6 * 2));
+            max = (max / 4) * 4;
+            int min = -max;
+            float numVerticalCells = 6 * (max / 4);
+            int totalVertices = max * 2 * 6 * 2;
+            var cellStrideY = gridLayout.cellGap.y + gridLayout.cellSize.y;
+            var cellOffsetY = gridLayout.cellSize.y / 2;
+            var hexOffset = (1.0f / 3.0f);
+            var drawTotal = numVerticalCells * 2.0f * hexOffset;
+            var drawDiagTotal = 2 * drawTotal;
+            Vector3[] vertices = new Vector3[totalVertices];
+            Vector2[] uvs2 = new Vector2[totalVertices];
+            // Draw Vertical Lines
+            for(int x = min; x < max; x++)
+            {
+                vertices[vertex] = gridLayout.CellToLocal(new Vector3Int(x, min, 0));
+                vertices[vertex + 1] = gridLayout.CellToLocal(new Vector3Int(x, max, 0));
+                uvs2[vertex] = new Vector2(0f, 2 * hexOffset);
+                uvs2[vertex + 1] = new Vector2(0f, 2 * hexOffset + drawTotal);
+                vertex += 2;
+                // Alternate Row Offset
+                vertices[vertex] = gridLayout.CellToLocal(new Vector3Int(x, min - 1, 0));
+                vertices[vertex + 1] = gridLayout.CellToLocal(new Vector3Int(x, max - 1, 0));
+                uvs2[vertex] = new Vector2(0f, 2 * hexOffset);
+                uvs2[vertex + 1] = new Vector2(0f, 2 * hexOffset + drawTotal);
+                vertex += 2;
+            }
+            // Draw Diagonals
+            for(int y = min; y < max; y++)
+            {
+                float drawDiagOffset = ((y + 1) % 3) * hexOffset;
+                var cellOffSet = Grid.Swizzle(gridLayout.cellSwizzle, new Vector3(0f, y * cellStrideY + cellOffsetY, 0.0f));
+                // Slope Up
+                vertices[vertex] = gridLayout.CellToLocal(new Vector3Int(Mathf.RoundToInt(1.5f * min), min, 0)) + cellOffSet;
+                vertices[vertex + 1] = gridLayout.CellToLocal(new Vector3Int(Mathf.RoundToInt(1.5f * max), max, 0)) + cellOffSet;
+                uvs2[vertex] = new Vector2(0f, drawDiagOffset);
+                uvs2[vertex + 1] = new Vector2(0f, drawDiagOffset + drawDiagTotal);
+                vertex += 2;
+                // Slope Down
+                vertices[vertex] = gridLayout.CellToLocal(new Vector3Int(Mathf.RoundToInt(1.5f * max), min, 0)) + cellOffSet;
+                vertices[vertex + 1] = gridLayout.CellToLocal(new Vector3Int(Mathf.RoundToInt(1.5f * min), max, 0)) + cellOffSet;
+                uvs2[vertex] = new Vector2(0f, drawDiagOffset);
+                uvs2[vertex + 1] = new Vector2(0f, drawDiagOffset + drawDiagTotal);
+                vertex += 2;
+                // Alternate Row Offset
+                vertices[vertex] = gridLayout.CellToLocal(new Vector3Int(Mathf.RoundToInt(1.5f * min) + 1, min, 0)) + cellOffSet;
+                vertices[vertex + 1] = gridLayout.CellToLocal(new Vector3Int(Mathf.RoundToInt(1.5f * max) + 1, max, 0)) + cellOffSet;
+                uvs2[vertex] = new Vector2(0f, drawDiagOffset);
+                uvs2[vertex + 1] = new Vector2(0f, drawDiagOffset + drawDiagTotal);
+                vertex += 2;
+                vertices[vertex] = gridLayout.CellToLocal(new Vector3Int(Mathf.RoundToInt(1.5f * max) + 1, min, 0)) + cellOffSet;
+                vertices[vertex + 1] = gridLayout.CellToLocal(new Vector3Int(Mathf.RoundToInt(1.5f * min) + 1, max, 0)) + cellOffSet;
+                uvs2[vertex] = new Vector2(0f, drawDiagOffset);
+                uvs2[vertex + 1] = new Vector2(0f, drawDiagOffset + drawDiagTotal);
+                vertex += 2;
+            }
+            var uv0 = new Vector2(k_GridGizmoDistanceFalloff, 0f);
+            var indices = new int[totalVertices];
+            var uvs = new Vector2[totalVertices];
+            var colors = new Color[totalVertices];
+            var normals = new Vector3[totalVertices];     // Normal channel stores the position of the other end point of the line.
+            var uvs3 = new Vector2[totalVertices];        // UV3 channel stores the UV2 value of the other end point of the line.
+
+            for(int i = 0; i < totalVertices; i++)
+            {
+                uvs[i] = uv0;
+                indices[i] = i;
+                colors[i] = color;
+                var alternate = i + ((i % 2) == 0 ? 1 : -1);
+                normals[i] = vertices[alternate];
+                uvs3[i] = uvs2[alternate];
+            }
+
+            mesh.vertices = vertices;
+            mesh.uv = uvs;
+            mesh.uv2 = uvs2;
+            mesh.uv3 = uvs3;
+            mesh.colors = colors;
+            mesh.normals = normals;
+            mesh.SetIndices(indices, MeshTopology.Lines, 0);
+            return mesh;
+        }
+#endif
 
     }
 }
