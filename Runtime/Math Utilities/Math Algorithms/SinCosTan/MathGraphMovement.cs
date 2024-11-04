841e680f889c02c6267ac740d05581756290c033
crowhound <jonathan.westfall1@gmail.com>
2024-11-04 12:07:03 -0600

    Added a WIP basic graph movement class.
    This class will be pulled apart to be put in a Graph Math class.
    The Graph Math class will contain utility and some extension methods for Graph based algorithms.

---
+++ Runtime/Math Utilities/Math Algorithms/SinCosTan/MathGraphMovement.cs
@@ -0,0 +1,64 @@
+using System;
+
+using UnityEditor;
+
+using UnityEngine;
+
+namespace SF.Pathfinding
+{
+	public enum TrigFunctionType
+	{
+		Sin,
+		Cos,
+		Tan
+	}
+
+	public class MathGraphMovement : MonoBehaviour
+    {
+		public Vector2 Speed = new Vector2(1,2);
+		public TrigFunctionType TrigMovementType;
+		public float Amplitude;
+
+		private Transform _transform;
+		private Vector3 _startingPos;
+
+		public float YOffSet = 0;
+		private void Awake()
+		{	
+			_transform = GetComponent<Transform>();
+			_startingPos = _transform.position;
+		}
+
+		private void Update()
+		{
+			switch(TrigMovementType) 
+			{
+				case TrigFunctionType.Cos:
+				{
+						break;
+				}
+				case TrigFunctionType.Sin:
+				{
+
+
+
+						// y = sin(Angle) * Amplitude + Y Amplitude Offset
+						YOffSet = Mathf.Sin(Time.time) * Amplitude;
+						Vector3 targetPos = new Vector3(_startingPos.x, _startingPos.y + YOffSet, _startingPos.z);
+						transform.position = Vector3.Lerp(_startingPos, targetPos, Mathf.Abs(YOffSet) * Speed.y);
+						break;
+				}
+			}
+
+		}
+
+		private void OnDrawGizmosSelected()
+		{
+			Handles.color = Color.yellow;
+			Vector3 tangentStart = new Vector3(transform.position.x, transform.position.y + Amplitude,transform.position.z);
+			Vector3 tangentEnd = new Vector3(transform.position.x, transform.position.y - Amplitude,transform.position.z);
+
+			Handles.DrawBezier(transform.position, transform.up, tangentStart, tangentEnd,Color.yellow,null,5);
+		}
+	}
+}
